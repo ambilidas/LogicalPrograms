d1e513c7108b36b76963f6802b952761246f9c09
Ambili das v s <ambili.kan@gmail.com>
2022-05-09 12:21:43 +0530

    Logical programs is added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Welcome to Logical Programs");
+           FibonacciSeries obj1 = new FibonacciSeries();
+            obj1.fib();
+
+        }
+    }
+
+
+}
